ec6ff655ab03abea16bc45183147601e9262c87c
kazuooooo <matsumotokazuya7@gmail.com>
2017-11-25 15:32:28 +0900

    add react

---
+++ app/javascript/packs/lockers/components/lockers.jsx
@@ -0,0 +1,47 @@
+// Run this example by adding <%= javascript_pack_tag 'hello_react' %> to the head of your layout file,
+// like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
+// of the page.
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
+import Locker from './locker'
+
+class Lockers extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+
+  renderLocker(i) {
+    return <Locker value={i} />;
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderLocker(0)}
+          {this.renderLocker(1)}
+          {this.renderLocker(2)}
+        </div>
+        <div className="board-row">
+          {this.renderLocker(3)}
+          {this.renderLocker(4)}
+          {this.renderLocker(5)}
+        </div>
+        <div className="board-row">
+          {this.renderLocker(6)}
+          {this.renderLocker(7)}
+          {this.renderLocker(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Lockers
---
+++ app/javascript/packs/lockers/show_app.jsx
@@ -0,0 +1,25 @@
+// Run this example by adding <%= javascript_pack_tag 'hello_react' %> to the head of your layout file,
+// like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
+// of the page.
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
+import Lockers from './components/lockers'
+
+class ShowApp extends React.Component {
+  render() {
+    return (
+      <div className="wrapper">
+        <Lockers />
+      </div>
+    );
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <ShowApp />,
+    document.body.appendChild(document.createElement('div')),
+  )
+})
